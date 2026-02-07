1455d7fd2d05534c5dbd9bcd5560e9e30b8f8826
MayankShukla01 <mayank2005shukla@gmail.com>
2026-02-08 02:13:25 +0530

    Rename to index.html

--- server.js
+++ server.js
@@ -42,7 +42,6 @@ app.post("/chat", async (req, res) => {
         });
     }
 });
-app.use(express.static("public"));
 app.listen(3000, () => {
     console.log("Server running on port 3000 🚀");
 });
